9489166eb10ff33bb14e0ffb2183ebc8b23011c4
Andrew <andrei@m407>
2021-03-31 15:15:14 +0300

    init

---
+++ src/test/java/ru/m407/stock/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.m407.stock.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
